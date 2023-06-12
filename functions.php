8a0b3b5be37c0032b51503e972e49830802e2668
tradesouthwest <tradesouthwest@gmail.com>
2023-06-12 11:06:50 -0700

    added ob fix

--- functions.php
+++ functions.php
@@ -405,3 +405,16 @@ function classic_sixteen_theme_widget_tag_cloud_args( $args ) {
 
 	return $args;
 }
+
+/**
+ * Proper ob_end_flush() for all levels
+ *
+ * This replaces the WordPress `wp_ob_end_flush_all()` function
+ * with a replacement that doesn't cause PHP notices.
+ */
+if (defined('WP_DEBUG') && true === WP_DEBUG) :
+    remove_action( 'shutdown', 'wp_ob_end_flush_all', 1 );
+    add_action( 'shutdown', function() {
+    while ( @ob_end_flush() );
+    } );
+endif; 
