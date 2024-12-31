6cb23bfeb3a400acb00b8fac2793d0d116b4715b
Larry <tradesouthwest@gmail.com>
2024-12-31 15:03:39 -0700

    push htm5 fix

--- functions.php
+++ functions.php
@@ -121,19 +121,21 @@ function classic_sixteen_theme_setup() {
 		/*
 		 * Switch default core markup for search form, comment form, and comments
 		 * to output valid HTML5.
-		 *//* 
-		add_theme_support(
-			'html5',
-			array(
-				'search-form',
-				'comment-form',
-				'comment-list',
-				'gallery',
-				'caption',
-				'script',
-				'style',
-			)
-		); */
+		 */
+		if ( function_exists( 'is_classicpress' ) && version_compare( '2.0', $cp_version, '<' ) ) {
+			add_theme_support(
+				'html5',
+				array(
+					'search-form',
+					'comment-form',
+					'comment-list',
+					'gallery',
+					'caption',
+					'script',
+					'style',
+				)
+			); 
+		}
 
 		/*
 		 * Enable support for Post Formats.
